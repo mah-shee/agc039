f7cc4e49052f6253e8f07bcfb08155ee206246c4
masa <masatoshi.sboard@gmail.com>
2020-09-15 16:49:27 +0900

    unfinished a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        s: Chars,
+        k: usize,
     }
+    let mut skip = false;
+    let mut count = 0;
+    for i in 0..s.len() - 1 {
+        if skip {
+            skip = false;
+            continue;
+        }
+        if s[i] == s[i + 1] {
+            count += 1;
+            skip = true;
+        }
+    }
+    count = count * k;
+    if s[0] == s[s.len() - 1] && skip == false {
+        count += (k - 1) / 2;
+    }
+    println!("{}", count);
 }
